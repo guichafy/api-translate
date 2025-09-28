f36dcb4e51b61c31866269c42d97e6f2a9a198b2
Guilherme Chafy <guichafy@gmail.com>
2025-09-27 23:55:49 -0300

    refactor(dto): convert TranslateResponse class to record
    Simplify data transfer object by using Java record feature which automatically provides constructor, getters, equals, hashCode and toString methods

--- src/main/java/sample_bedrock/translate/dto/TranslateResponse.java
+++ src/main/java/sample_bedrock/translate/dto/TranslateResponse.java
@@ -7,32 +7,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Response da tradução de termos")
-public class TranslateResponse {
-
+public record TranslateResponse(
     @Schema(description = "Lista de termos traduzidos", example = "[\"Hi, Chafy\", \"How are you?\"]", required = true)
     @JsonProperty("terms_translated")
-    private List<String> termsTranslated;
-
-    // Constructors
-    public TranslateResponse() {}
-
-    public TranslateResponse(List<String> termsTranslated) {
-        this.termsTranslated = termsTranslated;
-    }
-
-    // Getters and Setters
-    public List<String> getTermsTranslated() {
-        return termsTranslated;
-    }
-
-    public void setTermsTranslated(List<String> termsTranslated) {
-        this.termsTranslated = termsTranslated;
-    }
-
-    @Override
-    public String toString() {
-        return "TranslateResponse{" +
-                "termsTranslated=" + termsTranslated +
-                '}';
-    }
-}
+    List<String> termsTranslated
+) {}
